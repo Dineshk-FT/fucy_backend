5ebe72962b2d6d4f62ffcf99857601edd29a65b4
DineshRK11 <dineshfa7@gmail.com>
2025-05-30 12:27:34 +0530

    Initial commit without secrets

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ class Config:
     SECRET_KEY = "your_secret_key"
     FLASK_ENV = "development"
     MONGO_URI = "mongodb://mongo:27017/MY_DB"
-    AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=fucytechdocs;AccountKey=+MpE5EQsABQbMW+HnS0vj1PqXbWc2AzBEeKwzMbPNz4S3lXPfkoxFv5m2rUj2y3GXpbxInJucWH7+AStJSYK5w==;EndpointSuffix=core.windows.net"
+    AZURE_CONNECTION_STRING = "AZURE_CONNECTION_STRING"
     AZURE_CONTAINER_NAME = "documents"
-    GENAI_API_KEY = "AIzaSyDHYoIyp48qUtq1czC0C3DwddggtP73Osw"
+    GENAI_API_KEY = "GENAI_API_KEY"
