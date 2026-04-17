1f5b2bc999bd6300e62f4e13ae48310fb743e134
DineshRK11 <dineshfa7@gmail.com>
2026-04-18 01:27:00 +0530

    added waitress to run for longer load

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 from app import create_app
-# from waitress import serve
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    # serve(app, host='0.0.0.0', port=5000)
-# if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5001, debug=True, use_reloader=True)
+    serve(
+        app,
+        host='127.0.0.1',
+        port=5001,
+        channel_timeout=600,      # 10 minutes
+        connection_limit=1000,
+        cleanup_interval=30,
+        recv_bytes=1024**2,       # 1MB
+        send_bytes=1024**2,       # 1MB
+        url_scheme='http'
+    )
